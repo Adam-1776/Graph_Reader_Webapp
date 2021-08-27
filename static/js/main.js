f6d0c8839d8c27036b1ee4f25c96ae4927f56654
Adam Rizk <adamrizk9@gmail.com>
2021-08-27 11:29:08 +0530

    Added Matrix Table

--- static/js/main.js
+++ static/js/main.js
@@ -37,6 +37,40 @@ samp2.onclick = function(){
   document.querySelector('.file-name').textContent = "";
 }
 
+const samp3 = document.querySelector("#samp3");
+samp3.onclick = function(){
+  let mySrc = samp3.getAttribute('src');
+  const hiddenImg = document.querySelector("#hiddenImg");
+  const defaultImg = document.querySelector("#defaultImg");
+  defaultImg.setAttribute ('src',mySrc);
+  hiddenImg.setAttribute ('value','graph_def3.png')
+  if(nameOfFile==""){
+    nameOfFile='graph_def3.png';}
+  document.querySelector("#file").removeAttribute('required');
+  document.querySelector('.file-name').textContent = "";
+}
+
+function processMatrix(matrix){
+  console.log(JSON.stringify(matrix));
+  nodeArray = matrix.node_names;
+  for(let i=0;i<matrix.num_nodes;++i)
+    console.log(nodeArray[i]);
+  let area = document.getElementById("analysis");
+  let string = '<table><tr><th>Nodes</th>';
+  for(let i=0;i<matrix.num_nodes;++i)
+    string += '<th>'+nodeArray[i]+'</th>';
+  string += '</tr>';
+  for(let i=0;i<matrix.num_nodes;++i){
+    string += '<tr><td>'+nodeArray[i]+'</td>';
+    for(let j=0;j<matrix.num_nodes;++j){
+       string += '<td>'+matrix.matrix[i][j]+'</td>';
+    }
+    string += '</tr>';
+  }
+  
+  area.innerHTML = string;
+}
+
 function getMatrix(parameter){
   if(parameter!='none')
     {nameOfFile=parameter;
@@ -47,7 +81,6 @@ function getMatrix(parameter){
     return response.json();
   })
   .then(function (text) {
-    console.log('GET response:');
-    console.log(JSON.stringify(text)); 
+    processMatrix(text); 
 });
 }
